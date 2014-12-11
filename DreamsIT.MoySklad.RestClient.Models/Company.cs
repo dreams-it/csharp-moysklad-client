96a8acd51a020826887088bf8fa2493980c642f1
wqIpw <trolbibo@gmail.com>
2014-12-11 16:34:20 +0200

    Add company models

---
+++ DreamsIT.MoySklad.RestClient.Models/Company.cs
@@ -0,0 +1,14 @@
+﻿using DreamsIT.MoySklad.RestClient.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamsIT.MoySklad.RestClient.Models
+{
+    public class Company : Organization
+    {
+        public bool PayerVat { get; set; }
+    }
+}

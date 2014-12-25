4e1719342a14b937c638f6ace186c7c60a1ab4c7
wqIpw <trolbibo@gmail.com>
2014-12-25 16:48:15 +0200

    update IMsRestApiDbContext

--- DreamsIT.MoySklad.DataAccess/Abstracts/IMsRestApiDbContext.cs
+++ DreamsIT.MoySklad.DataAccess/Abstracts/IMsRestApiDbContext.cs
@@ -8,31 +8,31 @@ using System.Threading.Tasks;
 
 namespace DreamsIT.MoySklad.DataAccess.Abstracts
 {
-    class IMsRestApiDbContext
+    public interface IMsRestApiDbContext
     {
-        public DbSet<Good> Goods { get; set; }
-        public DbSet<CashOut> CashOut { get; set; }
-        public DbSet<Company> Company { get; set; }
-        public DbSet<CustomerOrder> CustomerOrder { get; set; }
-        public DbSet<Demand> Demand { get; set; }
-        public DbSet<Employee> Employee { get; set; }
-        public DbSet<Enter> Enter { get; set; }
-        public DbSet<EnterPosition> EnterPosition { get; set; }
-        public DbSet<GoodFolder> GoodFolder { get; set; }
-        public DbSet<GoodRef> GoodRef { get; set; }
-        public DbSet<Loss> Loss { get; set; }
-        public DbSet<MyCompany> MyCompany { get; set; }
-        public DbSet<PaymentIn> PaymentIn { get; set; }
-        public DbSet<PaymentOut> PaymentOut { get; set; }
-        public DbSet<Price> Price { get; set; }
-        public DbSet<PurchaseOrder> PurchaseOrder { get; set; }
-        public DbSet<PurchaseOrderPosition> PurchaseOrderPosition { get; set; }
-        public DbSet<RetailDemand> RetailDemand { get; set; }
-        public DbSet<RetailStore> RetailStore { get; set; }
-        public DbSet<Supply> Supply { get; set; }
-        public DbSet<Warehouse> Warehouse { get; set; }
-        public DbSet<CustomerOrderPosition> CustomerOrderPosition { get; set; }
+        DbSet<Good> Goods { get; set; }
+        DbSet<CashOut> CashOut { get; set; }
+        DbSet<Company> Company { get; set; }
+        DbSet<CustomerOrder> CustomerOrder { get; set; }
+        DbSet<Demand> Demand { get; set; }
+        DbSet<Employee> Employee { get; set; }
+        DbSet<Enter> Enter { get; set; }
+        DbSet<EnterPosition> EnterPosition { get; set; }
+        DbSet<GoodFolder> GoodFolder { get; set; }
+        DbSet<GoodRef> GoodRef { get; set; }
+        DbSet<Loss> Loss { get; set; }
+        DbSet<MyCompany> MyCompany { get; set; }
+        DbSet<PaymentIn> PaymentIn { get; set; }
+        DbSet<PaymentOut> PaymentOut { get; set; }
+        DbSet<Price> Price { get; set; }
+        DbSet<PurchaseOrder> PurchaseOrder { get; set; }
+        DbSet<PurchaseOrderPosition> PurchaseOrderPosition { get; set; }
+        DbSet<RetailDemand> RetailDemand { get; set; }
+        DbSet<RetailStore> RetailStore { get; set; }
+        DbSet<Supply> Supply { get; set; }
+        DbSet<Warehouse> Warehouse { get; set; }
+        DbSet<CustomerOrderPosition> CustomerOrderPosition { get; set; }
 
-        public DbSet<ShipmentIn> ShipmentIns { get; set; }
+        DbSet<ShipmentIn> ShipmentIns { get; set; }
     }
 }

162cf3e8ea43e2dda283ff342fc47e4247c7a983
wqIpw <trolbibo@gmail.com>
2015-01-30 16:45:42 +0200

    add model PriceType

---
+++ DreamsIT.MoySklad.RestClient.Models/PriceType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamsIT.MoySklad.RestClient.Models
+{
+    /// <summary>
+    /// Цена розница с картой
+    /// </summary>
+    class PriceType
+    {
+        public DateTime Updated { get; set; }
+        public string UpdatedBy { get; set; }
+        public Guid AccountUuid { get; set; }
+        public Guid AccountId { get; set; }
+        public Guid Uuid { get; set; }
+    }
+}
